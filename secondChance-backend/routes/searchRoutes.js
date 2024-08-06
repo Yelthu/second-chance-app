072be01271e62f4fd02a47d228120a951cd9a33e
Yelthu <88573155+Yelthu@users.noreply.github.com>
2024-08-06 17:18:08 +0700

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+require('dotenv').config();
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
-
-        const collection = db.collection("gifts");
-
+        const db = await connectToDatabase();
+        const collection = db.collection(process.env.MONGO_COLLECTION);
         // Initialize the query object
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
+        if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
+        }
 
-        // Task 3: Add other filters to the query
+        // Add other filters to the query
         if (req.query.category) {
-            // {{insert code here}}
+            query.category = req.query.category;
         }
         if (req.query.condition) {
-            // {{insert code here}} 
+            query.condition = req.query.condition;
         }
         if (req.query.age_years) {
-            // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
-
+        const gifts = await collection.find(query).toArray();
         res.json(gifts);
     } catch (e) {
         next(e);
