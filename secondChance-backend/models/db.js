50b05d6d5eefaf196770fe2c8ffabb1b53b1e021
Yelthu <88573155+Yelthu@users.noreply.github.com>
2024-08-06 17:17:25 +0700

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -19,7 +19,7 @@ async function connectToDatabase() {
     await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    const dbInstance = client.db(dbName);
+    dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
     return dbInstance;
