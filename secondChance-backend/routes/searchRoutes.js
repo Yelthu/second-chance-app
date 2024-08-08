97538dcaa41593aaccd6d8dae1152d2eb21e2b77
Yelthu <88573155+Yelthu@users.noreply.github.com>
2024-08-08 10:19:49 +0700

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,37 +1,38 @@
-const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-require('dotenv').config();
+const express = require('express')
+const router = express.Router()
+const connectToDatabase = require('../models/db')
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection(process.env.MONGO_COLLECTION);
-        // Initialize the query object
-        let query = {};
+  try {
+    // Connect to the database
+    const db = await connectToDatabase()
+    const collection = db.collection('secondChanceItems')
 
-        // Add the name filter to the query if the name parameter is not empty
-        if (req.query.name && req.query.name.trim() !== '') {
-            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        }
+    // Initialize the query object
+    const query = {}
 
-        // Add other filters to the query
-        if (req.query.category) {
-            query.category = req.query.category;
-        }
-        if (req.query.condition) {
-            query.condition = req.query.condition;
-        }
-        if (req.query.age_years) {
-            query.age_years = { $lte: parseInt(req.query.age_years) };
-        }
-
-        const gifts = await collection.find(query).toArray();
-        res.json(gifts);
-    } catch (e) {
-        next(e);
+    // Add the name filter to the query if the name parameter is not empty
+    if (req.query.name && req.query.name.trim() !== '') {
+      query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
-});
+    // Add other filters to the query
+    if (req.query.category) {
+      query.category = req.query.category
+    }
+    if (req.query.condition) {
+      query.condition = req.query.condition
+    }
+    if (req.query.age_years) {
+      query.age_years = { $lte: parseInt(req.query.age_years) }
+    }
 
-module.exports = router;
+    // Task 4: Fetch filtered items using the find(query) method
+    const gifts = await collection.find(query).toArray()
+    res.json(gifts)
+  } catch (e) {
+    next(e)
+  }
+})
+
+module.exports = router
