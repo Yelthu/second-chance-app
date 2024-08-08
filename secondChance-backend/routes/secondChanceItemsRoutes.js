44ae488a222bc1287b153762deae576c9621078e
Yelthu <88573155+Yelthu@users.noreply.github.com>
2024-08-08 09:29:46 +0700

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -11,12 +11,12 @@ const directoryPath = 'public/images'
 
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
-    destination:function(req, file, cb) {
-     cb(null, directoryPath) // Specify the upload directory
-    },
-    filename:function(req, file, cb) {
-     cb(null, file.originalname) // Use the original file name
-    },
+  destination: function (req, file, cb) {
+    cb(null, directoryPath) // Specify the upload directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) // Use the original file name
+  },
 });
 
 const upload = multer({ storage: storage })
